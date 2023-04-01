9d20e67cbfd4932ce92ec0459d4d7235816aba48
Santiago C <scoronado@integrupo.com>
2023-03-31 23:59:31 -0500

    created project in local

---
+++ Farmacia/Farmacia/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Farmacia.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
